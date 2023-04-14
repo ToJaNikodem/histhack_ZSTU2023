df15fe0bc2c5bce00cad3ba6c3a704d560a83448
tojanikodem <nikodembuczkowski05@gmail.com>
2023-04-14 13:33:52 +0200

    123

--- strona/app.js
+++ strona/app.js
@@ -11,7 +11,7 @@ window.addEventListener("scroll", ()=>{
 
 let menu = document.querySelector(".menu");
 let icon = document.querySelector(".icon");
-let exit = document.querySelector(".menu span");
+let exit = document.querySelector(".span");
 icon.addEventListener("click", ()=>{
     menu.classList.add('menu_a'); 
     menu.style.display="block";
@@ -19,3 +19,31 @@ icon.addEventListener("click", ()=>{
 exit.addEventListener("click", ()=>{
     menu.classList.remove('menu_a');
 });
+
+
+
+
+
+
+const animateClass = document.querySelectorAll('p');
+
+function ElementInView(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  animateClass.forEach((element) => {
+    if (ElementInView(element)) {
+      element.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('load', handleScroll);
+window.addEventListener('scroll', handleScroll);
